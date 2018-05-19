9322181c247aaadbf8c3ab676733bcd167eede26
devops <devops@localhost>
2018-05-19 22:50:35 +1000

    test associate route table

--- route/public_route.tf
+++ route/public_route.tf
@@ -13,11 +13,15 @@ output "Public_routeTB_id" {
   value = "${aws_route_table.Public.id}"
 }
 
-resource "aws_route_table_association" "public"{
-  subnet_id = [
-    "${var.public_subnet_1_id}",
-    "${var.public_subnet_2_id}",
-    "${var.public_subnet_3_id}"
-  ]
+resource "aws_route_table_association" "public1"{
+  subnet_id = "${var.public_subnet_1_id}"
+  route_table_id = "${aws_route_table.Public.id}"
+}
+resource "aws_route_table_association" "public2"{
+  subnet_id = "${var.public_subnet_2_id}"
+  route_table_id = "${aws_route_table.Public.id}"
+}
+resource "aws_route_table_association" "public3"{
+  subnet_id = "${var.public_subnet_3_id}"
   route_table_id = "${aws_route_table.Public.id}"
 }
